cc1567466d815b8220bd9f56cb2f034a156d619f
Zephram0 <31057196+Zephram0@users.noreply.github.com>
2023-03-22 04:10:07 -0400

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const chatForm = document.getElementById("chat-form");
+  const userInput = document.getElementById("user-input");
+  const chatBox = document.getElementById("chat-box");
+
+  chatForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const message = userInput.value.trim();
+    if (message === "") return;
+
+    displayUserMessage(message);
+    userInput.value = "";
+
+    try {
+      const response = await fetch("https://your-backend-url.com/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message }),
+      });
+
+      if (!response.ok) throw new Error("Failed to fetch GPT response");
+
+      const data = await response.json();
+      displayGptMessage(data.message);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  });
+
+  function displayUserMessage(message) {
+    const messageElement = document.createElement("div");
+    messageElement.classList.add("chat-message", "user-message");
+    messageElement.textContent = `You: ${message}`;
+    chatBox.appendChild(messageElement);
+    chatBox.scrollTop = chatBox.scrollHeight;
+  }
+
+  function displayGptMessage(message) {
+    const messageElement = document.createElement("div");
+    messageElement.classList.add("chat-message", "gpt-message");
+    messageElement.textContent = `GPT: ${message}`;
+    chatBox.appendChild(messageElement);
+    chatBox.scrollTop = chatBox.scrollHeight;
+  }
+});
